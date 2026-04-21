2ab88b85f1f2e0b51136094aa2a760fb466d841b
Evandro <evandrojramos@gmail.com>
2026-04-21 01:48:52 -0300

    feat: ajuste para deploy

--- src/app/notificacoes.py
+++ src/app/notificacoes.py
@@ -4,6 +4,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
